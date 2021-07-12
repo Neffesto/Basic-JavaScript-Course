903932dd1aef23e6245e9a15b61d27fb81c7eea7
Ruslan <BorodinRA@Yandex.ru>
2021-07-13 00:22:48 +0300

    Домашнее задание 2 урока (не готовое)

---
+++ lesson 2/arithmetic_operations.js
@@ -0,0 +1,48 @@
+/*
+5. Реализовать основные 4 арифметические операции в виде функций с двумя параметрами. Обязательно использовать оператор return.
+*/
+
+//let a = +prompt('введите число a');
+//let b = +prompt('введите число b');
+
+/*summation(a, b);
+
+function summation(a, b) {
+    var sum = (a + b);
+    alert(sum);
+    return (a, b);
+}
+
+subtraction(a, b);
+
+function subtraction(a, b) {
+    var sub = (a - b);
+    alert(sub);
+    return (a, b);
+}
+
+multiplication(a, b);
+
+function multiplication(a, b) {
+    var sub = (a * b);
+    alert(sub);
+    return (a, b);
+}
+
+division(a, b)
+
+function division(a, b) {
+    var divis = (a / b);
+    alert(divis);
+    return (a, b);
+}*/
+
+function division(a, b) {
+    return (a / b);
+}
+console.log(division(10, 5));
+
+function summation(a, b) {
+    return (a * b);
+}
+console.log(division(10, 5));
